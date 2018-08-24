926c06f10a54dfac39a36d7c8544bc1f091f907c
ferreusveritas <ferreusveritas@gmail.com>
2018-08-24 16:57:39 -0400

    BiomeDataBasePopulator work

--- src/main/java/com/ferreusveritas/dtphc/BiomeDataBasePopulator.java
+++ src/main/java/com/ferreusveritas/dtphc/BiomeDataBasePopulator.java
@@ -1,13 +1,15 @@
 package com.ferreusveritas.dtphc;
 
-import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.EnumChance;
-import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.IChanceSelector;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.ISpeciesSelector;
+import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.RandomSpeciesSelector;
 import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.SpeciesSelection;
 import com.ferreusveritas.dynamictrees.api.worldgen.IBiomeDataBasePopulator;
 import com.ferreusveritas.dynamictrees.trees.Species;
 import com.ferreusveritas.dynamictrees.worldgen.BiomeDataBase;
-import com.ferreusveritas.dynamictrees.worldgen.BiomeDataBase.Operation;
 import com.ferreusveritas.dynamictrees.worldgen.TreeGenerator;
 import com.pam.harvestcraft.HarvestCraft;
 import com.pam.harvestcraft.config.TreeGenerationConfiguration;
@@ -15,30 +17,34 @@ import com.pam.harvestcraft.config.TreeGenerationConfiguration;
 import net.minecraft.world.biome.Biome;
 
 public class BiomeDataBasePopulator implements IBiomeDataBasePopulator {
-
+	
+	Map<Biome, RandomSpeciesSelector> biomeMap = new HashMap<>();
+	
 	@Override
 	public void populate() {
 		
 		BiomeDataBase dbase = TreeGenerator.getTreeGenerator().biomeDataBase;
 		
 		for(TreeGenerationConfiguration treeConfig : HarvestCraft.fruitTreeConfigManager.treeConfigurations) {
-			String fruitName = treeConfig.getFruit();
-			Species species = ModTrees.phcSpecies.get(fruitName);
-			if(species != null) {
-				for(Biome biome : treeConfig.getBiomesAllowed()) {
-					if(dbase.shouldCancelVanillaTreeGen(biome)) {//Only populate biomes with Dynamic Trees that have been cancelled
-						dbase.setChanceSelector(biome, computeChance(biome), Operation.SPLICE_BEFORE);
-						dbase.setSpeciesSelector(biome, computeSpecies(biome), Operation.SPLICE_BEFORE);
+			if(treeConfig.getEnableGeneration()) {
+				String fruitName = treeConfig.getFruit();
+				Species species = ModTrees.phcSpecies.get(fruitName);
+				if(species != null) {
+					for(Biome biome : treeConfig.getBiomesAllowed()) {
+						if(dbase.shouldCancelVanillaTreeGen(biome)) {//Only populate biomes with Dynamic Trees that have been cancelled
+							RandomSpeciesSelector selector = biomeMap.computeIfAbsent(biome, b -> new RandomSpeciesSelector());
+							selector.add(species, 100 - treeConfig.getRarity());
+							//dbase.setSpeciesSelector(biome, computeSpecies(biome), Operation.SPLICE_BEFORE);
+						}
 					}
 				}
 			}
 		}
 
-	}
-
-	public IChanceSelector computeChance(Biome biome) {
-		//TODO: Set up chances for PHC trees
-		return (rnd, spc, rad) -> { return EnumChance.UNHANDLED; };
+		for(Entry<Biome, RandomSpeciesSelector> e : biomeMap.entrySet()) {
+			
+		}
+		
 	}
 
 	public ISpeciesSelector computeSpecies(Biome biome) {
