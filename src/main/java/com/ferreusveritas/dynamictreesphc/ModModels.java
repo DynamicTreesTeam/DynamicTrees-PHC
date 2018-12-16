f24005b8d932dd62edd79bdd019e60b6ef06382f
ferreusveritas <ferreusveritas@gmail.com>
2018-12-16 15:12:47 -0500

    Fix model registry looking for minecraft:air

--- src/main/java/com/ferreusveritas/dynamictreesphc/ModModels.java
+++ src/main/java/com/ferreusveritas/dynamictreesphc/ModModels.java
@@ -1,11 +1,9 @@
 package com.ferreusveritas.dynamictreesphc;
 
 import com.ferreusveritas.dynamictrees.api.client.ModelHelper;
-import com.ferreusveritas.dynamictrees.blocks.BlockDynamicLeaves;
-import com.ferreusveritas.dynamictrees.blocks.LeavesPaging;
+import com.ferreusveritas.dynamictrees.items.Seed;
 import com.ferreusveritas.dynamictrees.trees.TreeFamily;
 
-import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -21,12 +19,7 @@ public class ModModels {
 			ModelHelper.regModel(tree);//Register custom state mapper for branch
 		}
 		
-		//Register GrowingLeavesBlocks Meshers and Colorizers
-		for(BlockDynamicLeaves leaves: LeavesPaging.getLeavesMapForModId(ModConstants.MODID).values()) {
-			ModelHelper.regModel(Item.getItemFromBlock(leaves));
-		}
-		
-		ModTrees.phcSpecies.values().forEach(species -> ModelHelper.regModel(species.getSeed()));//Register Seed Item Models
+		ModTrees.phcSpecies.values().stream().filter(s -> s.getSeed() != Seed.NULLSEED).forEach(s -> ModelHelper.regModel(s.getSeed()));//Register Seed Item Models
 	}
 	
 }
